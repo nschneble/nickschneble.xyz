0c1d88d766ec0243cad8843444b5ad48188e2f6f
Nick Schneble <nschneble@users.noreply.github.com>
2025-06-24 16:28:53 +0100

    first homepage pass

---
+++ shortcuts.js
@@ -0,0 +1,9 @@
+document.addEventListener('keypress', (event) => {
+	switch(event.key) {
+		case '3':
+			document.location.href = 'https://www.linkedin.com/in/nschneble/';
+			break;
+		default:
+			// do nothing
+	}
+});
