19a3fef7687e26c4b7938496ba1169900b4da3be
Nick Schneble <nschneble@users.noreply.github.com>
2025-06-26 19:09:41 +0100

    move html to its own folder

--- build.sh
+++ build.sh
@@ -2,17 +2,16 @@
 rm -rf public
 
 mkdir public
-mkdir public/accolades
-mkdir public/backstory
-mkdir public/stumble
-
-cp accolades/index.html public/accolades/index.html
-cp backstory/index.html public/backstory/index.html
-cp stumble/index.html public/stumble/index.html
-cp index.html public/index.html
 cp links.json public/links.json
 cp robots.txt public/robots.txt
 cp sitemap.xml public/sitemap.xml
+
+# create the html folder structure
+mkdir public/accolades public/backstory public/stumble
+cp html/accolades.html public/accolades/index.html
+cp html/backstory.html public/backstory/index.html
+cp html/index.html public/index.html
+cp html/stumble.html public/stumble/index.html
 
 mkdir public/css
 sass css/style.scss public/css/style.css
